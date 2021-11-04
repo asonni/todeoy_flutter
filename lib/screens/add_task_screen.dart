fd411c1b20de6ad176211950ef1012918c849a34
Aladdin Sonni <aladdin.sonni@gmail.com>
2021-11-04 09:30:30 +0200

    Complete Design

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF757575),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            topLeft: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (newText) {},
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            // ignore: deprecated_member_use
+            const FlatButton(
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.lightBlueAccent,
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
